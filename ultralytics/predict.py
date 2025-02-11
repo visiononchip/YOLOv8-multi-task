6e3055580a514da57a3f244eda71439adaf4218d
Juhyun Kim <jhkim@visiononchip.com>
2025-02-11 10:09:36 +0000

    localize prediction configs for lambdalabs instance

--- ultralytics/predict.py
+++ ultralytics/predict.py
@@ -1,9 +1,9 @@
 import sys
-sys.path.insert(0, "/home/jiayuan/ultralytics-main/ultralytics")
+sys.path.insert(0, "/home/ubuntu/voc-us-west/YOLOv8-multi-task/ultralytic")
 
 from ultralytics import YOLO
 
 
 number = 3 #input how many tasks in your work
-model = YOLO('/home/jiayuan/ultralytics-main/ultralytics/runs/best.pt')  # Validate the model
-model.predict(source='/data/jiayuan/dash_camara_dataset/daytime', imgsz=(384,672), device=[3],name='v4_daytime', save=True, conf=0.25, iou=0.45, show_labels=False, save_txt=True)
+model = YOLO('/home/ubuntu/voc-us-west/YOLOv8-multi-task/runs/multi/train_demo_output7/weights/best.pt')  # Validate the model
+model.predict(source='/home/ubuntu/voc-us-west/train_test/images/val2017', imgsz=(384,672), device=[0],name='predict_demo', save=True, conf=0.25, iou=0.45, show_labels=False, save_txt=True)
