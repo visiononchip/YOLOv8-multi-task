2e3d691518b0d8ca4f89d94ab193a1800bbcfed4
Juhyun Kim <jhkim@visiononchip.com>
2025-02-11 10:00:08 +0000

    localize training configs for lambdalabs instance

--- ultralytics/train.py
+++ ultralytics/train.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.insert(0, "/home/jiayuan/yolom/ultralytics")
+sys.path.insert(0, "/home/ubuntu/voc-us-west/YOLOv8-multi-task/ultralytics")
 # 现在就可以导入Yolo类了
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('/home/jiayuan/yolom/ultralytics/models/v8/yolov8-bdd-v4-one-dropout-individual-n.yaml', task='multi')  # build a new model from YAML
 # model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 # model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+model = YOLO('/home/ubuntu/voc-us-west/base_model_720p.pt', task='multi')
 
 # Train the model
-model.train(data='/home/jiayuan/yolom/ultralytics/datasets/bdd-multi.yaml', batch=96, epochs=100, imgsz=(640,640), device=0, name='train_demo_output', val=True, task='multi',classes=[0,1,2,3,4,5,6],combine_class=[],single_cls=False, save_period=5, workers=16, resume=False)
+model.train(data='/home/ubuntu/voc-us-west/YOLOv8-multi-task/ultralytics/datasets/bdd-multi.yaml', batch=96, epochs=100, imgsz=(640,640), device=0, name='train_demo_output', val=True, task='multi',classes=[0,1,2,3,4,5,6],combine_class=[],single_cls=False, save_period=5, workers=16, resume=False)
