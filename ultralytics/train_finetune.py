7196f377965e0366ae3926c2419acd24d281b2f9
Juhyun Kim <jhkim@visiononchip.com>
2025-02-13 12:00:17 +0000

    apply geekyb config on train.py base&finetune variants

--- ultralytics/train_finetune.py
+++ ultralytics/train_finetune.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.insert(0, "/home/jiayuan/yolom/ultralytics")
+sys.path.insert(0, "/home/ubuntu/YOLOv8-multi-task/ultralytics")
 # 现在就可以导入Yolo类了
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('/home/jiayuan/yolom/ultralytics/models/v8/yolov8-bdd-v4-one-dropout-individual-n.yaml', task='multi')  # build a new model from YAML
 # model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 # model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+model = YOLO('../base_model_720.pt', task='multi')
 
 # Train the model
-model.train(data='/home/jiayuan/yolom/ultralytics/datasets/bdd-multi.yaml', batch=12, epochs=300, imgsz=(640,640), device=[0,1,2], name='yolopm', val=True, task='multi',classes=[2,3,4,9,10,11],combine_class=[2,3,4,9],single_cls=True)
+model.train(data='./datasets/bdd-multi-voc-finetune.yaml', batch=96, epochs=100, imgsz=(640,640), device=0, name='train_finetune_output', val=True, task='multi',classes=[0,1,2,3,4,5,6],combine_class=[],single_cls=False, save_period=5, workers=16, resume=False)
