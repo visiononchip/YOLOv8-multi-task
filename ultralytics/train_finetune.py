9fc94a292ef0ea4879223f890e63a0fc411a43f1
Juhyun Kim <jhkim@visiononchip.com>
2025-02-19 14:10:19 +0000

    update train_finetune.py for 1080p

--- ultralytics/train_finetune.py
+++ ultralytics/train_finetune.py
@@ -13,7 +13,7 @@ from ultralytics import YOLO
 # Load a model
 # model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 # model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
-model = YOLO(project_root+'/pretrained/ayololms_geekyb_720p.pt', task='multi')
+model = YOLO(project_root+'/pretrained/ayolom-voc-1080-20250218.pt', task='multi')
 
 # Train the model
-model.train(data=source_root+'/ultralytics/datasets/bdd-multi-voc-finetune.yaml', batch=96, epochs=100, imgsz=(640,640), device=0, name='train_finetune_output', val=True, task='multi',classes=[0,1,2,3,4,5,6],combine_class=[],single_cls=False, save_period=5, workers=16, resume=False)
+model.train(data=source_root+'/ultralytics/datasets/bdd-multi-voc-finetune.yaml', batch=30, epochs=100, imgsz=(1080,1080), device=0, name='train_finetune_output', val=True, task='multi',classes=[0,1,2,3,4,5,6],combine_class=[],single_cls=False, save_period=5, workers=16, resume=False)
