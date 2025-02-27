3f15417dd5eb33f192ec6d5da5941faae6178300
Juhyun Kim <jhkim@visiononchip.com>
2025-02-27 07:56:19 +0000

    add lr0 and increase epochs for finetune training

--- ultralytics/train_finetune.py
+++ ultralytics/train_finetune.py
@@ -16,4 +16,4 @@ from ultralytics import YOLO
 model = YOLO(project_root+'/pretrained/ayolom-voc-1080-20250218.pt', task='multi')
 
 # Train the model
-model.train(data=source_root+'/ultralytics/datasets/bdd-multi-voc-finetune.yaml', batch=30, epochs=100, imgsz=(1080,1080), device=0, name='train_finetune_output', val=True, task='multi',classes=[0,1,2,3,4,5,6],combine_class=[],single_cls=False, save_period=5, workers=16, resume=False)
+model.train(data=source_root+'/ultralytics/datasets/bdd-multi-voc-finetune.yaml', batch=30, epochs=200, imgsz=(1080,1080), device=0, name='train_finetune_output', val=True, task='multi',classes=[0,1,2,3,4,5,6],combine_class=[],single_cls=False, save_period=5, workers=16, resume=False, lr0=0.003)
